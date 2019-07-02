a2316e13f9b7a565aaf1f0ffd0bfdc62703c587c
Conor Owens <conorj.owens@gmail.com>
2019-07-02 02:25:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
   def self.first_X_students_in_grade_10(x)
     arr = []
     i = 1
-    until i == 10
+    until i == x
       self.all.map do |student|
         arr << student
         i += 1
